c05012aa18dec20e77adda3e5a8e296ff0d09130
Tarık Demirtaş <tarik.demirtas@arvento.com>
2020-02-12 18:17:22 +0300

    Web Sockets Copied from Youtube

---
+++ BeholderSockets/SocketsManager/ConnectionManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BeholderSockets.SocketsManager
+{
+    public class ConnectionManager
+    {
+        private ConcurrentDictionary<string, WebSocket> _connections = new ConcurrentDictionary<string, WebSocket>();
+
+        public WebSocket GetSocketById(string id)
+        {
+            return _connections.FirstOrDefault(connection => connection.Key == id).Value;
+        }
+
+        public ConcurrentDictionary<string, WebSocket> GetAllConnections()
+        {
+            return _connections;
+        }
+
+        public string GetId(WebSocket socket)
+        {
+            return _connections.FirstOrDefault(connection => connection.Value.Equals(socket)).Key;
+        }
+
+        public async Task RemoveSocketAsync(string id)
+        {
+            _connections.TryRemove(id, out var socket);
+
+            await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Socket Connection Closed", CancellationToken.None);
+        }
+
+        public void AddSocket(WebSocket socket)
+        {
+            _connections.TryAdd(GetConnectionId(),socket);
+        }
+        private string GetConnectionId()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+    }
+}
---
+++ BeholderSockets/SocketsManager/SocketsExtension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BeholderSockets.SocketsManager
+{
+    public static class SocketsExtension
+    {
+        public static IServiceCollection AddSocketManager(this IServiceCollection services) 
+        {
+            services.AddTransient<ConnectionManager>();
+            foreach (var type in Assembly.GetEntryAssembly().ExportedTypes)
+            {
+                if(type.GetTypeInfo().BaseType == typeof(SocketHandler))
+                {
+                    services.AddSingleton(type);
+                }
+            }
+
+            return services;
+        }
+
+        public static IApplicationBuilder MapSockets(this IApplicationBuilder app, PathString path, SocketHandler socket)
+        {
+            return app.Map(path, (x) => x.UseMiddleware<SocketMiddleware>(socket));
+        }
+    }
+}
